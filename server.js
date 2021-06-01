83a89eb901ac4c2832d258c3feecbca6816a212d
akshadagrawal <akshadagrawal.aa@gmail.com>
2021-06-02 00:51:35 +0530

    Prepared to deploy

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ if(process.env.NODE_ENV === "production"){
 
 
 // connection
-const port= process.env.port || 6000;
+const port= process.env.PORT || 6000;
 app.listen(port,()=>{
     console.log("Connected"); 
 })    
