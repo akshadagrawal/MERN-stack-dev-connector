12eebcdd45897b840b38d7156a98815a3c0c1a17
akshadagrawal <akshadagrawal.aa@gmail.com>
2021-06-02 00:43:22 +0530

    Prepared to deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const users= require('./routes/api/users');
 const profile= require('./routes/api/profile');
 const posts= require('./routes/api/posts');
-
+const path= require('path');
 const mongoURI= require('./config/keys').mongoURI;
 const app= express();
 
@@ -29,6 +29,16 @@ app.use(express.json());
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+//server static assets if in prod
+if(process.env.NODE_ENV === "production"){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 
 
